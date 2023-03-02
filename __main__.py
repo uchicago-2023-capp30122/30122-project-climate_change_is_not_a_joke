f3ad57373a32042755bce82e55ccafbb99f26a1d
Robert C Surridge <rcsurridge@linux6.cs.uchicago.edu>
2023-03-02 11:53:22 -0600

    Customized dashboard port

--- __main__.py
+++ __main__.py
@@ -270,4 +270,4 @@ def update_table(country):
     return data_table_filter.to_dict("records")
 
 if __name__ == "__main__":
-    app.run_server()
+    app.run_server(debug = True, host = '0.0.0.0', port = 8051)
