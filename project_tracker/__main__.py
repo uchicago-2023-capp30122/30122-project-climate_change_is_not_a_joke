7c07f8e156e7b6180119901689d543719334f381
Robert C Surridge <rcsurridge@linux6.cs.uchicago.edu>
2023-03-07 14:32:48 -0600

    Updated style for my modules

--- project_tracker/__main__.py
+++ project_tracker/__main__.py
@@ -15,7 +15,7 @@ from project_tracker.inputs import header_style, small_dropdown_style, \
                                 paris_filter, source_filter, map_dd_options, \
                                 map_dd_style, hl_reg_style, hl_reg_layout_style, \
                                 country_options, hl_data_table_options, hl_data_table_text, \
-                                hl_data_table_cell,  bar_filter, bar_options, ll_data_table_cell
+                                bar_filter, bar_options, ll_data_table_cell
 from project_tracker.load_and_clean.load_data import hl_df, ll_df
 from project_tracker.graphs.starting_graphs import logreg_fig, linreg_fig, hist_fig, \
                                           map_fig, scatter_fig, bar_fig, avg_commit_fig
@@ -26,125 +26,123 @@ from project_tracker.graphs.starting_graphs import logreg_fig, linreg_fig, hist_
 app.layout = dbc.Container([
     html.Br(),
     dbc.Row(html.H1("Climate Change Investment Tracker"), 
-                    style = header_style,
-                    justify = 'center'),
+                    style=header_style,
+                    justify='center'),
     html.Br(),
 
     dbc.Row(html.H3("Interactive Map: Tracking the Impact of the Paris Agreement on Climate Change Investment"), 
-                    style = header_style,
-                    justify = "center"),
+                    style=header_style,
+                    justify="center"),
 
     html.Br(),
 
     html.Br(),
 
-    html.Div(children = [dbc.Row([dbc.Col([
+    html.Div(children=[dbc.Row([dbc.Col([
                     
                     dbc.Card([                    
                     html.H5("Filter: Pre/Post Paris Agreement"),
-                    dcc.Dropdown(id = "primary_map_filter",
-                                 style = big_dropdown_style,
-                                 options = paris_filter),
+                    dcc.Dropdown(id="primary_map_filter",
+                                 style=big_dropdown_style,
+                                 options=paris_filter),
                     
                     html.H5("Filter: Funding Source"), 
-                    dcc.Dropdown(id = "secondary_map_filter",
-                                 style = big_dropdown_style,
-                                 options = source_filter),
+                    dcc.Dropdown(id="secondary_map_filter",
+                                 style=big_dropdown_style,
+                                 options=source_filter),
                     
                     html.H5("Dropdown: Project Breakdown by Country"), 
-                    dcc.Dropdown(id = "map_dd",
-                                 style = big_dropdown_style,
-                                 options = map_dd_options)],
-                                 style = map_dd_style,
-                                 color = "#D2E5D0")]),
+                    dcc.Dropdown(id="map_dd",
+                                 style=big_dropdown_style,
+                                 options=map_dd_options)],
+                                 style=map_dd_style,
+                                 color="#D2E5D0")]),
 
-                    dbc.Col([(dcc.Graph(id = 'map',
-                                        figure = map_fig,
-                                        style = plot_style))])])]),
+                    dbc.Col([(dcc.Graph(id='map',
+                                        figure=map_fig,
+                                        style=plot_style))])])]),
     html.Br(),
 
     dbc.Row(html.H3("Static Regressions: Examining the Effect of the Paris Agreement \
                     on Climate Change Project Investment"), 
-                    style = header_style,
-                    justify = "center"),
+                    style=header_style,
+                    justify="center"),
     html.Br(),
 
-    html.Div(children = 
-             [html.Div(dcc.Graph(id = 'logreg_fig', 
-                                 figure = logreg_fig,
-                                 style = plot_style), 
-                                 style = hl_reg_style),
+    html.Div(children=[html.Div(dcc.Graph(id='logreg_fig', 
+                                          figure=logreg_fig,
+                                          style=plot_style), 
+                                          style=hl_reg_style),
             
-              html.Div(dcc.Graph(id = "linreg_fig",
-                                 figure = linreg_fig,
-                                 style = plot_style), 
-                                 style = hl_reg_style)],
-                                 style = hl_reg_layout_style),  
+                      html.Div(dcc.Graph(id="linreg_fig",
+                                 figure=linreg_fig,
+                                 style=plot_style), 
+                                 style=hl_reg_style)],
+                                 style=hl_reg_layout_style),  
     dbc.Row(html.Br()),
     html.Br(),
 
     dbc.Row(html.H3("Static Histograms: Examining the Distribution of Climate Change \
                      Project Funding"), 
-                    style = header_style,
-                    justify = "center"),
+                    style=header_style,
+                    justify="center"),
     
     html.Br(),
 
-    html.Div(children = 
-             [html.Div(dcc.Graph(id = "hist_fig",
-                                  figure = hist_fig,
-                                  style = plot_style),
-                                  style = hl_reg_style),
+    html.Div(children=[html.Div(dcc.Graph(id="hist_fig",
+                                          figure=hist_fig,
+                                          style=plot_style),
+                                          style=hl_reg_style),
             
-              html.Div(dcc.Graph(id = "hist_fig2",
-                                 figure = avg_commit_fig,
-                                 style = plot_style), 
-                                 style = hl_reg_style)],
-                                 style = hl_reg_layout_style),  
+                       html.Div(dcc.Graph(id="hist_fig2",
+                                          figure=avg_commit_fig,
+                                          style=plot_style), 
+                                          style=hl_reg_style)],
+                                          style=hl_reg_layout_style),  
     dbc.Row(html.Br()),
     dbc.Row(html.Br()),
     html.Br(),
     
     dbc.Row(html.H2("Climate Change Project Deep Dive by Country"), 
-                    style = header_style,
-                    justify = "center"),
+                    style=header_style,
+                    justify="center"),
     html.Br(),
 
     dbc.Row(html.H4("Primary Filter: Select a Country"), 
-                    style = header_style,
-                    justify = "center"),
+                    style=header_style,
+                    justify="center"),
 
-    dbc.Row(dcc.Dropdown(id = "country_dd",
-                         options = country_options,
-                         multi = False,
-                         style = small_dropdown_style),
-                         justify = "center"),
+    dbc.Row(dcc.Dropdown(id="country_dd",
+                         options=country_options,
+                         multi=False,
+                         style=small_dropdown_style),
+                         justify="center"),
     html.Br(),
 
     dbc.Row(html.H4("Secondary Filter: Select Another Country to Compare"), 
-                    style = header_style,
-                    justify = "center"),
+                    style=header_style,
+                    justify="center"),
     
-    dbc.Row(dcc.Dropdown(id = "second_country_dd",
-                         options = country_options,
-                         multi = False,
-                         style = small_dropdown_style),
-                         justify = "center"),
+    dbc.Row(dcc.Dropdown(id="second_country_dd",
+                         options=country_options,
+                         multi=False,
+                         style=small_dropdown_style),
+                         justify="center"),
     html.Br(),
     
     dbc.Row(html.H3("Data Table: Comparison of Country GDP and Climate Vulnerability"), 
-                    style = header_style,
-                    justify = "center"),
+                    style=header_style,
+                    justify="center"),
     
-    dbc.Row(dash_table.DataTable(id = "hl_data_table",
-                                 data = hl_df.to_dict("records"),
-                                 columns = hl_data_table_options,
-                                 page_current = 0,
-                                 page_size = 5,
-                                 style_data = hl_data_table_text,
-                                 style_cell = ll_data_table_cell,
-                                 sort_action = "native",
-                                 sort_mode = "multi")),
+    dbc.Row(dash_table.DataTable(id="hl_data_table",
+                                 data=hl_df.to_dict("records"),
+                                 columns=hl_data_table_options,
+                                 page_current=0,
+                                 page_size=5,
+                                 style_data=hl_data_table_text,
+                                 style_cell=ll_data_table_cell,
+                                 sort_action="native",
+                                 sort_mode="multi")),
     html.Br(),
 
     dbc.Row("The ND-GAIN Country Index summarizes a country's vulnerability to \
@@ -161,60 +159,59 @@ app.layout = dbc.Container([
     html.Br(),
 
     dbc.Row(html.H3("Graphs on Climate Change Projects for Selected Country"), 
-                    style = header_style,
-                    justify = "center"),
+                    style=header_style,
+                    justify="center"),
 
     html.Div(html.Br()),
 
     html.Div(html.H5("Category Filter: Project Breakdown"), 
-                     style = hl_reg_layout_style),
+                     style=hl_reg_layout_style),
     
-    html.Div(dcc.Dropdown(id = "bar_x_filter",
-                          options = bar_options,
-                          multi = False),
-                          style = bar_filter),
+    html.Div(dcc.Dropdown(id="bar_x_filter",
+                          options=bar_options,
+                          multi=False),
+                          style=bar_filter),
     html.Div(html.Br()),
 
     html.Div(html.H5("Color Filter: Project Breakdown"), 
-                     style = hl_reg_layout_style),
+                     style=hl_reg_layout_style),
     
-    html.Div(dcc.Dropdown(id = "bar_color_filter",
-                          options = bar_options,
-                          multi = False),
-                          style = bar_filter),
+    html.Div(dcc.Dropdown(id="bar_color_filter",
+                          options=bar_options,
+                          multi=False),
+                          style=bar_filter),
     html.Br(),
     html.Br(),
 
-    html.Div(children = 
-             [html.Div(dcc.Graph(id = "bar", 
-                                 figure = bar_fig,
-                                 style = plot_style), 
-                                 style = hl_reg_style),
+    html.Div(children=[html.Div(dcc.Graph(id="bar", 
+                                          figure=bar_fig,
+                                          style=plot_style), 
+                                          style=hl_reg_style),
             
-              html.Div(dcc.Graph(id = "ll_scatter",
-                                 figure = scatter_fig,
-                                 style = plot_style), 
-                                 style = hl_reg_style)],
-                                 style = hl_reg_layout_style),  
+                       html.Div(dcc.Graph(id="ll_scatter",
+                                          figure=scatter_fig,
+                                          style=plot_style), 
+                                          style=hl_reg_style)],
+                                          style=hl_reg_layout_style),  
     html.Br(),
     html.Br(),
     html.Br(),
 
     dbc.Row(html.H3("Data Table: Projects for Selected Country"), 
-                    style = header_style,
-                    justify = "center"),
+                    style=header_style,
+                    justify="center"),
     html.Br(),
 
-    dbc.Row(dash_table.DataTable(id = "ll_data_table",
-                                 data = ll_df.to_dict('records'), 
-                                 columns = bottom_table_cols,
-                                 page_current = 0,
-                                 page_size = 10,
-                                 style_data = hl_data_table_text,
-                                 style_cell = ll_data_table_cell,
-                                 sort_action = "native",
-                                 filter_action = "native",
-                                 sort_mode = "multi")),
+    dbc.Row(dash_table.DataTable(id="ll_data_table",
+                                 data=ll_df.to_dict('records'), 
+                                 columns=bottom_table_cols,
+                                 page_current=0,
+                                 page_size=10,
+                                 style_data=hl_data_table_text,
+                                 style_cell=ll_data_table_cell,
+                                 sort_action="native",
+                                 filter_action="native",
+                                 sort_mode="multi")),
     html.Br(),
     html.Br(),
 
@@ -222,7 +219,7 @@ app.layout = dbc.Container([
 
     html.Br()],
 
-fluid = True, style = {"backgroundColor": "#D2E5D0"})
+fluid=True, style={"backgroundColor": "#D2E5D0"})
 
 
 ## Update Figures
@@ -297,19 +294,20 @@ def update_map(primary_filter, secondary_filter, dd):
 
     # Construct the updated map
     map_fig = px.choropleth(new_df,
-                            locations = "Country", 
-                            locationmode = "country names",
-                            color = color_filter,
-                            color_continuous_scale = px.colors.diverging.BrBG,
-                            scope = "asia",
-                            title = f"{title_label}")
+                            locations="Country", 
+                            locationmode="country names",
+                            color=color_filter,
+                            color_continuous_scale=px.colors.diverging.BrBG,
+                            scope="asia",
+                            title=f"{title_label}")
 
-    map_fig.update_coloraxes(colorbar_orientation = "h", colorbar_len = .7)
-    map_fig.update_geos(center = {"lat": 28, "lon": 87})
-    map_fig.update_layout(coloraxis_colorbar_y = -0.2,
-                          paper_bgcolor = "#D2E5D0",
-                          title_x = 0.5)
-    map_fig.update_layout(margin = {"l":20, "r":12, "t":40, "b":40, "pad": 10})
+    map_fig.update_coloraxes(colorbar_orientation="h", 
+                             colorbar_len=.7)
+    map_fig.update_geos(center={"lat": 28, "lon": 87})
+    map_fig.update_layout(coloraxis_colorbar_y=-0.2,
+                          paper_bgcolor="#D2E5D0",
+                          title_x=0.5)
+    map_fig.update_layout(margin={"l":20, "r":12, "t":40, "b":40, "pad": 10})
 
     return map_fig
 
@@ -358,18 +356,18 @@ def update_scatter(country):
 
     # Construct the updated scatterplot
     fig = px.scatter(scatter_df, 
-                     x = "Year",
-                     y = "Project Count by Year",
-                     size = "Project Commitment Amount by Year",
-                     title = f"Project Count Over Time in {country}",
-                     labels = {"Commitment Amount": "Commitment Amount"},
-                     trendline = "ols")
+                     x="Year",
+                     y="Project Count by Year",
+                     size="Project Commitment Amount by Year",
+                     title=f"Project Count Over Time in {country}",
+                     labels={"Commitment Amount": "Commitment Amount"},
+                     trendline="ols")
     
-    fig.add_vline(x = 2016, 
-                  line_dash = "dash", 
-                  line_color = "darkgreen",
-                  annotation_text = "Paris Agreement Ratification", 
-                  annotation_position = "top left")
+    fig.add_vline(x=2016, 
+                  line_dash="dash", 
+                  line_color="darkgreen",
+                  annotation_text="Paris Agreement Ratification", 
+                  annotation_position="top left")
     
     return fig
 
@@ -392,11 +390,11 @@ def update_bar(country, x_filter, color_filter):
 
     # Construct the updated bar plot
     fig = px.bar(bar_data_filter, 
-                 x = x_filter, 
-                 y = "Commitment Amount",
-                 color = color_filter,
-                 title = f"Commitment Amount by {x_filter} in {country}",
-                 hover_name = "Project Name")
+                 x=x_filter, 
+                 y="Commitment Amount",
+                 color=color_filter,
+                 title=f"Commitment Amount by {x_filter} in {country}",
+                 hover_name="Project Name")
     
     return fig
 
@@ -417,4 +415,4 @@ def update_ll_table(country):
 
 
 if __name__ == "__main__":
-    app.run_server(debug = True, host = "0.0.0.0", port = 3004)
+    app.run_server(debug=True, host="0.0.0.0", port=3004)
--- project_tracker/graphs/starting_graphs.py
+++ project_tracker/graphs/starting_graphs.py
@@ -11,24 +11,23 @@ hist_fig = hist_data()
 avg_commit_fig = avg_commitment_plot()
 
 map_fig = px.choropleth(hl_df,
-                        locations = "Country", 
-                        locationmode = "country names",
-                        scope = 'asia')
+                        locations="Country", 
+                        locationmode="country names",
+                        scope='asia')
 
-map_fig.update_geos(resolution = 110)
-map_fig.update_layout(paper_bgcolor = "#D2E5D0")
+map_fig.update_geos(resolution=110)
+map_fig.update_layout(paper_bgcolor="#D2E5D0")
 
 scatter_fig = px.scatter(ll_df, 
-                         x = "Effective Date", 
-                         y = "Commitment Amount",
-                         title = "Project Investment over Time",
-                         labels = {"Commitment Amount": "Commitment Amount"},
-                         trendline = "ols")
+                         x="Effective Date", 
+                         y="Commitment Amount",
+                         title="Project Investment over Time",
+                         labels={"Commitment Amount": "Commitment Amount"},
+                         trendline="ols")
 
 bar_fig = px.bar(ll_df, 
-                 x = "Status", 
-                 y = "Commitment Amount",
-                 #color = "Sector",
-                 title = "Project Breakdown",
-                 labels = {"Commitment Amount": "Commitment Amount",
-                           "Status": "Project Status"})
+                 x="Status", 
+                 y="Commitment Amount",
+                 title="Project Breakdown",
+                 labels={"Commitment Amount": "Commitment Amount",
+                         "Status": "Project Status"})
