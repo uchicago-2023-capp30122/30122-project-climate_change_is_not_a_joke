e6e6fc616e8f71b7f896dfad8695bfeeb3d8b380
Robert C Surridge <rcsurridge@linux6.cs.uchicago.edu>
2023-03-07 13:22:06 -0600

    Updated column name call for stats functions

--- project_tracker/graphs/dataviz.py
+++ project_tracker/graphs/dataviz.py
@@ -9,17 +9,17 @@ def rdd_example():
     data = pd.read_csv('project_tracker/data/raw/wb_data.csv')
     cutoff_year = 2017
     data['treatment'] = np.where(data['Year'] >= cutoff_year, 1, 0)
-    bandwidth_opt = rdd.optimal_bandwidth(data['Commitment_Amount'], data['Year'], cut=cutoff_year)
+    bandwidth_opt = rdd.optimal_bandwidth(data['Commitment Amount'], data['Year'], cut=cutoff_year)
     print("Optimal bandwidth:", bandwidth_opt)
     data_rdd = rdd.truncated_data(data, 'Year', bandwidth_opt, cut=cutoff_year)
 
     plt.figure(figsize=(12, 8))
-    plt.scatter(data_rdd['Year'], data_rdd['Commitment_Amount'], facecolors='none', edgecolors='r')
+    plt.scatter(data_rdd['Year'], data_rdd['Commitment Amount'], facecolors='none', edgecolors='r')
     plt.xlabel('Year')
-    plt.ylabel('Commitment_Amount')
+    plt.ylabel('Commitment Amount')
     plt.axvline(x=cutoff_year, color='b')
     plt.savefig('rdd_plot.png')  # save plot to a separate file
-    model = rdd.rdd(data_rdd, 'Year', 'Commitment_Amount', cut=cutoff_year)
+    model = rdd.rdd(data_rdd, 'Year', 'Commitment Amount', cut=cutoff_year)
     print(model.fit().summary())
     
 rdd_example()
